429bb31568d2e211836f781ca6709c1a8f91ed79
MonserratGE <monserrat.garcia.espinoza32@gmail.com>
2022-02-17 12:53:37 -0600

    feat: Completación de AXIOS Y EJS en la carpeta 4

--- 4_EJS/routes/index.js
+++ 4_EJS/routes/index.js
@@ -1,9 +1,50 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var axios = require("axios");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get("/", function (req, res, next) {
+  res.render("index", {
+    title: "Inicio",
+    message: "Bienvenido",
+  });
+});
+
+router.get("/pokemon/:name", function (req, res, next) {
+  let { name } = req.params;
+  axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`).then((response) => {
+    console.log(response)
+    res.render("pokemon", {
+      title: name,
+      
+      data: response.data,
+    });
+  });
+});
+
+router.get("/todos", function (req, res, next) {
+  axios.get("https://pokeapi.co/api/v2/pokemon").then((response) => {
+    console.log(response.data);
+    res.render("todos", {
+      title: "Todos",
+      data: response.data,
+    });
+  });
 });
 
 module.exports = router;
+
+// axios({
+//   method: "get",
+//   url: "https://jsonplaceholder.typicode.com/todos/2",
+// }).then(function (response) {
+//   console.log(response.data);
+// });
+
+// axios({
+//   url: "https://jsonplaceholder.typicode.com/todos/2",
+// }).then(function (response) {
+//   console.log(response.data);
+// });
+
